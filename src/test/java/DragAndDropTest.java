a5ae142cfad82785dfd9a289e6a606c56c575d5c
mipkasnow <steamer08@mail.ru>
2021-12-25 19:28:48 +0300

    Home Work

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        clearBrowserCookies();
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
+        Configuration.browserSize = "1590x850";
+    }
+
+    @AfterAll
+    static void afterAll(){
+        closeWebDriver();
+    }
+
+    @Test
+    void dragAndDrop(){
+        open("/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+    }
+}
---
+++ src/test/java/SoftAssertionsTest.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        clearBrowserCookies();
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.browserSize = "1590x850";
+        open("");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        closeWebDriver();
+    }
+
+    @Test
+    void softAssertionsTest(){
+        $("[data-test-selector='nav-search-input']").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("a[id='wiki-tab']").click();
+        $("[id='wiki-body'] [href*='SoftAssertions']").should(appear);
+        $(byText("Soft assertions")).click();
+
+        $(byText("Using JUnit5 extend test class:")).parent().sibling(0).shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+    }
+}
